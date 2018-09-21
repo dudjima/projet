8589e2525a81df62b0b0944e3f048f80f2604e46
dudjima <36198494+dudjima@users.noreply.github.com>
2018-09-21 13:55:06 +0200

    version finalisé
    optimisation du code
    gestion des données récupérés avec une expression régulières

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+//config 
+$host = '192.168.168.121'; //host
+$port = '9000'; //port
+$null = NULL; //null var
+
+date_default_timezone_set('Europe/Paris');
+
+?>
--- s.php
+++ s.php
@@ -1,140 +1,94 @@
 <?php
-$host = '192.168.168.121'; //host
-$port = '9000'; //port
-$null = NULL; //null var
-date_default_timezone_set('Europe/Paris');
+require_once 'config.php';																													//On integre le fichier de parametres
 
-//$i=0;
-//Create TCP/IP sream socket
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-//reuseable port
-socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);																						//Create TCP/IP sream socket
+socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);																					//reuseable port
+socket_bind($socket, 0, $port);																												//bind socket to specified host
+socket_listen($socket);																														//listen to port
+$clients = array($socket);																													//create & add listning socket to the list
 
-//bind socket to specified host
-socket_bind($socket, 0, $port);
-
-//listen to port
-socket_listen($socket);
-
-//create & add listning socket to the list
-$clients = array($socket);
-
-//start endless loop, so that our script doesn't stop
-while (true) {
-	//manage multipal connections
-	$changed = $clients;
-	//returns the socket resources in $changed array
-	socket_select($changed, $null, $null, 0, 10);
+while (true) {																																//start endless loop, so that our script doesn't stop
+	$changed = $clients; 																													//manage multipal connections
+	socket_select($changed, $null, $null, 0, 10); 																							//returns the socket resources in $changed array
+		
+	if (in_array($socket, $changed)) {																										//check for new socket
+		$socket_new = socket_accept($socket); 																								//accept new socket
+		$header = socket_read($socket_new, 9999);								 															//read data sent by the socket
+		//echo "Header : ".$header."/\r\n/";
 	
-	//check for new socket
-	if (in_array($socket, $changed)) {
-		$socket_new = socket_accept($socket); //accept new socket
-		
-		$header = socket_read($socket_new, 9999); //read data sent by the socket
-		echo "Header : ".$header."/\r\n/";
-
-		if (strpos($header,'num')>0){ // il s'agit d'un socket venant d'un tel
-				//on cherche l'ip dans le header
-				preg_match('/[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}/', $header, $ip_recup);
-				$line = chop($line);			
-				
-				$lines = preg_split("/\r\n/", $header);
-
-					preg_match('~Y(.*?)Z~', $header, $matches1);
-					$num = substr($matches1[1], 4, 12);
-					$num = trim(str_replace('&','', $num));
-					$num = trim(str_replace('+33','0', $num));
-					$num= substr($num, 0, 2).' '.substr($num, 2, 2).' '.substr($num, 4, 2).' '.substr($num, 6, 2).' '.substr($num, 8, 2);
-					if(strlen($num)>9){	// si le numero est superieur à 9 c'est un appel valide sinon c'est un appel interne et on laisse          
-						if(strpos($header,'manque')>0){ //c'est un appel manqué
-							socket_getpeername($socket_new, $ip); //get ip address of connected socket
-							$response = mask(json_encode(array('pseudo'=>'Arnaud', 'type'=>'manque', 'message' =>'[MANQUE] de '.$num.' le '.date("d-m H:i").'<a href="https://go.mytiger.pro/index.php?action=UnifiedSearch&module=Home&search_onlyin=Accounts%2CContacts%2CLeads&query_string='.$num.'" target="blank"> lien </a>' ))); //prepare json data								
-							send_message1($response,$ip_recup[0]); //notifie tout les utilisateur
-						}else{
-							socket_getpeername($socket_new, $ip); //get ip address of connected socket
-							$response = mask(json_encode(array('pseudo'=>'Arnaud','type'=>'phone', 'message' =>'appel de '.$num.' le '.date("d-m H:i").'<script language="javascript">
-							window.open("https://go.mytiger.pro/index.php?action=UnifiedSearch&module=Home&search_onlyin=Accounts%2CContacts%2CLeads&query_string='.$num.'");</script> <a href="https://go.mytiger.pro/index.php?action=UnifiedSearch&module=Home&search_onlyin=Accounts%2CContacts%2CLeads&query_string='.$num.'" target="blank"> lien </a>' ))); //prepare json data								
-							send_message1($response, $ip_recup[0]); //notifie l'utilisateur  
-
-						}
-					}			
-		}else{ // il s'agit d'une connexion client
-			$clients[] = $socket_new; //add socket to client array si pas téléphone on ajoute la socket
-			$i=1;
-			perform_handshaking($header, $socket_new, $host, $port); //perform websocket handshake
-			socket_getpeername($socket_new, $ip); //get ip address of connected socket
-			//$response = mask(json_encode(array('type'=>'system', 'message'=>$ip.' connected'))); //prepare json data
-			//send_message($response); //notify all users about new connection
-			
-			//prepare data to be sent to client
-				//$response_text = mask(json_encode(array('type'=>'usermsg', 'name'=>'Arnaud', 'message'=>'Message'));
-				//send_message($response_text); //send data
+		if(preg_match_all("/(?<=\/\?|&)([^=]+)=([^&\s]*)/", $header, $matches) > 3){ 														// on a des valeurs récupéré et on les listes
+			$args = array();
+			foreach($matches[1] as $i => $arg) { $args[$arg] = $matches[2][$i];	}															//on recupere les arguments ip numero remote etat
+			$args['num'] = trim(str_replace('+33', '0',$args['num'])); 	
+			$args['num'] = substr($args['num'], 0, 2).' '.substr($args['num'], 2, 2).' '.substr($args['num'], 4, 2).' '.substr($args['num'], 6, 2).' '.substr($args['num'], 8, 2);
+			if($args['remote']<>'@vgw1.voxity.fr'){ //c'est un appel recu
+				switch($args['etat']){
+					case 'manque':
+						$response = mask(json_encode(array('pseudo'=>'Arnaud', 'type'=>'manque', 'message' =>'[MANQUE] de '.$args["num"].' le '.date("d-m H:i").'<a href="https://go.mytiger.pro/index.php?action=UnifiedSearch&module=Home&search_onlyin=Accounts%2CContacts%2CLeads&query_string='.$args["num"].'" target="blank"> lien </a>' ))); //prepare json data
+						send_message1($response, $args['ip']); 
+						break;
+					case 'pris';
+						$response = mask(json_encode(array('pseudo'=>'Arnaud','type'=>'phone', 'message' =>'Appel de '.$args["num"].' le '.date("d-m H:i").'<script language="javascript">
+						window.open("https://go.mytiger.pro/index.php?action=UnifiedSearch&module=Home&search_onlyin=Accounts%2CContacts%2CLeads&query_string='.$args["num"].'");</script> <a href="https://go.mytiger.pro/index.php?action=UnifiedSearch&module=Home&search_onlyin=Accounts%2CContacts%2CLeads&query_string='.$args["num"].'" target="blank"> lien </a>' ))); //prepare json data
+						send_message1($response, $args['ip']); 
+						break;
+					default:
+						break;
+				}
+			}
+		}else{ 																																//c'est une nouvelle connexion
+			$clients[] = $socket_new; 																										//Add socket to client array si pas téléphone on ajoute la socket
+			perform_handshaking($header, $socket_new, $host, $port); 																		//perform websocket handshake
+			socket_getpeername($socket_new, $ip); 
 		}
-		
-		//make room for new socket
-		$found_socket = array_search($socket, $changed);
+		$found_socket = array_search($socket, $changed); 																					//Make room for new socket
 		unset($changed[$found_socket]);
 	}
 	
-	//loop through all connected sockets
-	
-	//if ($i==1)
-	//{
-		foreach ($changed as $changed_socket) {	
-			
-			//check for any incomming data
-			while(socket_recv($changed_socket, $buf, 1024, 0) >= 1){
-				$received_text = unmask($buf); //unmask data
-				//print "AAAAAAAAAAAAAAAAA      ".$received_text;
-				$tst_msg = json_decode($received_text); //json decode 
-				$user_name = $tst_msg->name; //sender name
-				$user_message = $tst_msg->message; //message text
-				$user_color = $tst_msg->color; //color
-				
-				//prepare data to be sent to client
-				$response_text = mask(json_encode(array('type'=>'usermsg', 'name'=>$user_name, 'message'=>$user_message, 'color'=>$user_color)));
-				send_message($response_text); //send data
-				break 2; //exist this loop
-			}
-			
-			$buf = socket_read($changed_socket, 1024, PHP_NORMAL_READ);
-			if ($buf === false) { // check disconnected client
-				// remove client for $clients array
-				$found_socket = array_search($changed_socket, $clients);
-				socket_getpeername($changed_socket, $ip);
-				unset($clients[$found_socket]);
-				
-				//notify all users about disconnected connection
-				$response = mask(json_encode(array('type'=>'system', 'message'=>$ip.' disconnected')));
-				send_message($response);
-			}
+	foreach ($changed as $changed_socket) {																									//loop through all connected sockets
+		while(socket_recv($changed_socket, $buf, 1024, 0) >= 1){																			//check for any incomming data
+			$received_text = unmask($buf); 																									//unmask data
+			$tst_msg = json_decode($received_text); 																						//json decode 
+			$user_name = $tst_msg->name; 																									//sender name
+			$user_message = $tst_msg->message; 																								//message text
+			$user_color = $tst_msg->color; 																									//color
+			$response_text = mask(json_encode(array('type'=>'usermsg', 'name'=>$user_name, 'message'=>$user_message, 'color'=>$user_color)));//prepare data to be sent to client
+			send_message($response_text); 																									//send data
+			break 2; 																														//exit this loop
 		}
-	//}
+		$buf = socket_read($changed_socket, 1024, PHP_NORMAL_READ);
+		if ($buf === false) { 																												// check disconnected client	
+			$found_socket = array_search($changed_socket, $clients);																		// remove client for $clients array
+			socket_getpeername($changed_socket, $ip);
+			unset($clients[$found_socket]);			
+			$response = mask(json_encode(array('type'=>'system', 'message'=>$ip.' disconnected')));											//notify all users about disconnected connection
+			send_message($response);
+		}
+	}
 }
-// close the listening socket
-socket_close($socket);
+socket_close($socket);																														// close the listening socket
 
 function send_message($msg){
 	global $clients;
 	foreach($clients as $changed_socket){ 
-            socket_write($changed_socket,$msg,strlen($msg));
+         socket_write($changed_socket,$msg,strlen($msg));
 	}
 	return true;
 }
+
 function send_message1($msg,$ip_envoi){
 	global $clients;
-	foreach($clients as $changed_socket){
-        socket_getpeername($changed_socket, $ip);
-        echo 'ip recupéré : '.$ip_envoi.'ip comparé '. $ip ;
+	foreach($clients as $changed_socket){ 																									// On parcours les différentes sockets
+        @socket_getpeername($changed_socket, $ip); 																							// On recupère l'ip de la socket du tableau
         if($ip_envoi === $ip){
-            socket_write($changed_socket,$msg,strlen($msg));
+			socket_write($changed_socket,$msg,strlen($msg));																				// On ecrit le message dans le socket
+			break;																															// On a envoyé le message pas la peine de boucler
         }        
 	}
 	return true;
 }
 
-//Unmask incoming framed message
-function unmask($text) {
+function unmask($text) {																													//Unmask incoming framed message
 	$length = ord($text[1]) & 127;
 	if($length == 126) {
 		$masks = substr($text, 4, 4);
@@ -155,8 +109,7 @@ function unmask($text) {
 	return $text;
 }
 
-//Encode message for transfer to client.
-function mask($text){
+function mask($text){																														//Encode message for transfer to client.
 	$b1 = 0x80 | (0x1 & 0x0f);
 	$length = strlen($text);
 	
@@ -169,8 +122,8 @@ function mask($text){
 	return $header.$text;
 }
 
-//handshake new client.
-function perform_handshaking($receved_header,$client_conn, $host, $port){
+
+function perform_handshaking($receved_header,$client_conn, $host, $port){																	//handshake new client.
 	$headers = array();
 	$lines = preg_split("/\r\n/", $receved_header);
 	foreach($lines as $line){
